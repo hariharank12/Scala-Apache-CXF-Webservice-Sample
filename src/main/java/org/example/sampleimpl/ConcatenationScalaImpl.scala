0fa1f20e54f62e507753ded33c8acd3c989e7a10
Hariharan <hariharank12@gmail.com>
2012-12-26 07:06:17 +0530

    added override keyword to scala impl

--- src/main/java/org/example/sampleimpl/ConcatenationScalaImpl.scala
+++ src/main/java/org/example/sampleimpl/ConcatenationScalaImpl.scala
@@ -26,7 +26,7 @@ class ConcatenationScalaImpl extends Concatenation {
 	@RequestWrapper(localName = "stringConcatenation", targetNamespace = "http://www.example.org/sample/", className = "org.example.sample.StringConcatenation")
 	@ResponseWrapper(localName = "stringConcatenationResponse", targetNamespace = "http://www.example.org/sample/", className = "org.example.sample.StringConcatenationResponse")
 	@WebMethod(action = "http://www.example.org/sample/stringConcatenation")
-	def stringConcatenation(
+	override def stringConcatenation(
 			@WebParam(name = "s1", targetNamespace = "") s1: String,
 			@WebParam(name = "s2", targetNamespace = "") s2: String): String = {
 		val s:String = s1 + s2
@@ -41,7 +41,7 @@ class ConcatenationScalaImpl extends Concatenation {
 	@ResponseWrapper(localName = "numberConcatenationResponse", targetNamespace = "http://www.example.org/sample/", className = "org.example.sample.NumberConcatenationResponse")
 	@WebMethod(action = "http://www.example.org/sample/numberConcatenation")
 	//public int numberConcatenation(
-	def numberConcatenation(
+	override def numberConcatenation(
 			@WebParam(name = "i1", targetNamespace = "") i1: Int,
 			@WebParam(name = "i2", targetNamespace = "") i2: Int): Int = {
 		println("I am number concatenation from scala");
